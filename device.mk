50c3c483e53ba50acdcfab86321cf4624da36472
argraur <argraur12@yandex.ru>
2016-12-03 20:07:22 +0100

    PixelROM: Initial commit

---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES := \
+    device/asus/grouper/fstab.grouper:root/fstab.grouper \
+    device/asus/grouper/init.grouper.rc:root/init.grouper.rc
+
+# GApps stuff
+GAPPS_VARIANT := stock
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+# Exclude Editors packages due to big size!
+GAPPS_EXCLUDED_PACKAGES := EditorsDocs
+GAPPS_EXCLUDED_PACKAGES := EditorsSlides
+GAPPS_EXCLUDED_PACKAGES := EditorsSheets
+# Nexus 7 IS Wi-Fi only. Goodbye Google Dialer!
+GAPPS_EXCLUDED_PACKAGES := GoogleDialer
+# Use Pixel Prebuilt wallpapers instead!
+GAPPS_EXCLUDED_PACKAGES := Wallpapers
+
+# the actual meat of the device-specific product definition
+$(call inherit-product, device/asus/grouper/device-common.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carrier=wifi-only
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/asus/grouper/overlay
+$(call inherit-product, vendor/google/build/opengapps-packages.mk)
